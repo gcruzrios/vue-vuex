0fcc180ff696504c336e940cd8800c8c7d02390c
gcruzrios <gcruzr@gmail.com>
2021-04-10 18:53:44 -0600

    Fin de ejercicio Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <ListMemes/>
+    
+  </div>
+    
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListMemes from './components/ListMemes.vue'
+//import { useStore } from "vuex";
 
 export default {
+  components: { ListMemes },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
+  
+  
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,44 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    titleApp: "Memes",
+    memes:[],
   },
   mutations: {
+    setMemes(state,payload){
+      state.memes = payload;
+    }
   },
   actions: {
+    async getMemes({commit}, params){
+     try {
+       const response = await fetch("https://api.imgflip.com/get_memes");
+       const result = await response.json();
+
+       if(!params?.total){
+          //console.log("todos los memes");
+        
+          commit("setMemes", result.data.memes);
+       }else{
+          const resultTemp = [];
+          result.data.memes.forEach((meme, index)=>{
+              if(index < params.total){
+                resultTemp.push(meme);
+              }
+          });
+          commit("setMemes", resultTemp)
+          //console.log(`Cargando ${params.total} memes`)
+       }
+       
+       console.log(params);
+
+
+     } catch (error) {
+       console.log(error)
+     }  
+    }
   },
+  getters: {},
   modules: {
   }
 })
